78b1bb24feb11ce25363b82e7a26a2020e377e54
teoferizovic <ferizovic.teo@gmail.com>
2018-08-31 23:13:35 +0200

    Added new field(external_name) in tbl. products.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,3 +19,7 @@ class ProductController extends Controller
     	return \Response::json($products,201);
     }
 }
+
+
+//interesting query
+//select u.email, ui.user_id, count(*) as total from user_images as ui inner join users as u on ui.user_id=u.id group by ui.user_id ORDER BY `total` DESC ;
