00f183804dd13ad3069ed3f41e728a634f00c007
teoferizovic <ferizovic.teo@gmail.com>
2018-10-25 00:34:18 +0200

    Added new relationship between tbl orders and tbl payments.

---
+++ database/migrations/2018_10_24_221550_add_new_field_in_tbl_orders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewFieldInTblOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedInteger('payment_id')->nullable()->after('user_id');
+            $table->foreign('payment_id')->references('id')->on('payments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['payment_id']);
+            $table->dropColumn('payment_id');
+         });   
+
+    }
+}
