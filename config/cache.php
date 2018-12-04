c3208a5cee1dc1e689f3f706524946c5f63e2381
teoferizovic <ferizovic.teo@gmail.com>
2018-12-04 19:15:52 +0100

    Minnor changes when caching routes, caching in Redis.

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => env('CACHE_DRIVER', 'redis-mem'),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,6 +71,10 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
+        ],
+        'redis-mem' => [
+            'driver' => 'redis',
+            'connection' => 'model-cache',
         ],
 
     ],
